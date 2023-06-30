7140e7fce4d93bacdcb2df46f3e35e0f91ad4f8b
Kevin-av <kiavila@sudamericano.edu.ec>
2023-06-29 19:14:08 -0500

    Actualizacion
    1.- La imagen de inicio con la 6 y 7 imagen
    2.- Creacion de los cuadros de parrafos con el menu
    3.- Creacion de sistema para pasar de dialogos

--- sec.js
+++ sec.js
@@ -1,3 +1,25 @@
 function PlayAudio(){
     document.getElementById("musica2").play();
 }
+
+let contador = 0;
+
+function siguiente() {
+  contador++;
+
+  if (contador === 1) {
+    document.getElementById('parrafo').innerHTML = 'Miras como el religioso ora con todas sus fuerzas por ayuda divina, solo para darte cuenta que una viga se aloja en su vientre atrapandolo, nadie podrá ayudarlo.';
+  } else if (contador === 2) {
+    document.getElementById('parrafo').innerHTML = 'Tus pasos se detienen, no por ti, si no por una mano que te sujeta con desesperación, al mirar a la dueña de esta mano, la esperanza comenzó a surgir en los ojos de aquella chica, pero tu no ves eso, en lo que te concentras es como un muro de concreto está aplastando los pies de la chica.';
+  } else if (contador === 3) {
+    document.getElementById('parrafo').innerHTML = 'Es imposible salvarla, lo sabes, no hay nada en lo que puedas ayudarla, entonces haces lo único que puedes hacer, sigues caminando ignorando las súplicas de salvación que la chica grita desgarrando su garganta por el smog en el aire por la carne quemada, con cada paso que das sus súplicas se convierten en gritos, y los gritos en maldiciones, ella te desea la muerte por dejarla abandonada.';
+  } else if (contador === 4) {
+    document.getElementById('parrafo').innerHTML = 'Tus pensamientos se detienen, nada bueno sale recordando, por eso si caminando por este lugar, un lugar donde no hay algún sentido del tiempo, donde tu existencia no es más que una anormalidad, un sitio donde solo hay un infinito sin nada, no sabes el porque caminas, no tienes ambiciones.';
+  } else if (contador === 5) {
+    document.getElementById('parrafo').innerHTML = 'Lo que alguna vez fuiste ya no existe para utilizarlo como motor, aun así dudas en seguir caminando sabiendo que sería inutil, sin embargo sigues caminando ya que es lo único que puedes hacer, porque sabes que debe haber algo más allá de toda la nada que te rodea.';
+  } else if (contador === 6) {
+    document.getElementById('parrafo').style.display = 'none';
+    document.getElementById('opciones').style.display = 'block';
+    document.getElementById('siguiente').style.display = 'none'; 
+  }
+}
