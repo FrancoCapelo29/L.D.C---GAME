6d28fc09c706d3669e0312bb900822c82cf5dbfb
Kevin-av <116099021+Kevin-av@users.noreply.github.com>
2023-07-01 10:40:02 -0500

    comando de audio
    ocurrio un error donde el audio se reproducia automaticamente sin que haya presionado nada del comando de audio, asi que se cambio el nombre del archivo

--- ini.js
+++ ini.js
@@ -1,7 +1,10 @@
 confirm("¿Estás listo para adentrarte en la oscuridad y enfrentar tu destino?");    
-function PlayAudio(){
-    document.getElementById("musica1").play();
-}
+
 function redireccion(){
     window.location.href = 'prologo_Inferno/prologo.html';
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var audioPlayer = document.getElementById("audio-player");
+    audioPlayer.pause();
+});
