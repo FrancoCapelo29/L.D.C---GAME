f3c463a15cf676b5dfbdf5c23f4df4065c5c99c3
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-07-02 17:46:08 -0500

    Tets

---
+++ TESTS/objetos.js
@@ -0,0 +1,7 @@
+export const objetoTest={
+    C1:{
+        inicial:`palabras, palabritas, palabrotas`,
+        segundo:`sonsacar al fernando la susodicha
+        vacuna para el vacuno`
+    }
+}
